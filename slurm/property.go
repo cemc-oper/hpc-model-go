9d04edd7911cc1824c17476853558f49a19eafc1
perillaroc <perillaroc@gmail.com>
2020-01-07 09:28:25 +0800

    :construction: add JobStateProperty.

---
+++ slurm/property.go
@@ -0,0 +1,26 @@
+package slurm
+
+import hpcmodel "github.com/nwpc-oper/hpc-model-go"
+
+type JobState string
+
+type JobStateProperty struct {
+	Category hpcmodel.QueryCategory
+	Value    string
+	Text     string
+	Data     JobState
+}
+
+func (p *JobStateProperty) SetValue(value string) {
+	p.Value = value
+	p.Text = value
+	p.Data = JobState(value)
+}
+
+func (p *JobStateProperty) SetCategory(category hpcmodel.QueryCategory) {
+	p.Category = category
+}
+
+func (p *JobStateProperty) PropertyID() string {
+	return p.Category.ID
+}
---
+++ slurm/util.go
@@ -0,0 +1,19 @@
+package slurm
+
+import (
+	hpcmodel "github.com/nwpc-oper/hpc-model-go"
+)
+
+func CreatePropertyLessFunc(id string) hpcmodel.LessFunc {
+	return func(item1, item2 *hpcmodel.Item) bool {
+		p1 := item1.GetProperty(id)
+		p2 := item2.GetProperty(id)
+		switch prop1 := p1.(type) {
+		case *JobStateProperty:
+			prop2 := p2.(*JobStateProperty)
+			return prop1.Data < prop2.Data
+		default:
+			return hpcmodel.CreatePropertyLessFunc(id)(item1, item2)
+		}
+	}
+}
